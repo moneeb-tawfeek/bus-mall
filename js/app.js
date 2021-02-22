3d10aef24c8ce0e505f6f1bd05de6c5da9a31774
moneeb-tawfeek <moneeb.tawfeek@gmail.com>
2021-02-22 13:29:16 +0200

    finishing lab 11

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let leftImgI;
 let middleImgI;
 let rightImgI;
 let userAttemptsCounter = 0;
-let maxAttempt = 3;
+let maxAttempt = 26;
 
 function MallImg(name, source) {
     this.name = name;
@@ -53,22 +53,67 @@ function renderThreeImg() {
     middleImgI = makeRandomI();
     rightImgI = makeRandomI();
 
-
-    while (
-        leftImgI === middleImgI ||
-        leftImgI === rightImgI ||
-        middleImgI === leftImgI ||
-        middleImgI === rightImgI ||
-
-        rightImgI === middleImgI ||
-
-        rightImgI === leftImgI
-
-    ) {
+    do {
         middleImgI = makeRandomI();
         rightImgI = makeRandomI();
-        leftImgI = makeRandomI();
     }
+    while (leftImgI === middleImgI || leftImgI === rightImgI || middleImgI === rightImgI)
+
+
+
+
+    left.src = MallImg.allImg[leftImgI].source;
+    // left.src = MallImg.allImg[leftImgI].name;
+    middle.src = MallImg.allImg[middleImgI].source;
+    // middle.src = MallImg.allImg[middleImgI].name;
+    right.src = MallImg.allImg[rightImgI].source;
+    // right.src = MallImg.allImg[rightImgI].name;
+    // console.log(MallImg.allImg[leftImgI]);
+}
+renderThreeImg();
+
+
+
+
+left.addEventListener('click', handleUserClick);
+middle.addEventListener('click', handleUserClick);
+right.addEventListener('click', handleUserClick);
+function handleUserClick(event) {
+    userAttemptsCounter++;
+
+    if (userAttemptsCounter < maxAttempt) {
+
+        if (event.target.id === 'left-image') {
+            MallImg.allImg[leftImgI].votes++
+
+            console.log(event.target);
+        }
+        else if (event.target.id === 'middle-image') {
+            MallImg.allImg[middleImgI].votes++
+
+        }
+
+
+        else {
+            MallImg.allImg[rightImgI].votes++
+        }
+        renderThreeImg();
+    }
+
+    else {
+        let list = document.getElementById('results-list')
+        let mallResult;
+        for (let i = 0; i < MallImg.allImg.length; i++) {
+            mallResult = document.createElement('li')
+            list.appendChild(mallResult)
+            mallResult.textContent = MallImg.allImg[i].name + ' has' + MallImg.allImg[i].votes + 'votes'
+        }
+
+        left.removeEventListener('click', handleUserClick);
+        middle.removeEventListener('click', handleUserClick);
+        right.removeEventListener('click', handleUserClick);
+    }
+
 
 }
 
@@ -80,52 +125,7 @@ console.log(leftImgI);
 console.log(middleImgI);
 console.log(rightImgI);
 
+// makeRandomI();
 
-left.src = MallImg.allImg[leftImgI].source;
-middle.src = MallImg.allImg[middleImgI].source;
-right.src = MallImg.allImg[rightImgI].source;
-
-makeRandomI();
-renderThreeImg();
-
-
-
-left.addEventListener('click', handleUserClick);
-middle.addEventListener('click', handleUserClick);
-right.addEventListener('click', handleUserClick);
-function handleUserClick(event) {
-    userAttemptsCounter++;
-
-    if (userAttemptsCounter < maxAttempt) {
-        // make sure to add to votes for the correct element and render again
-        if (event.target.id === 'left-image') {
-            MallImg.allImg[leftImgI].votes++
-
-
-        }
-        else if (event.target.id === 'middle-image') {
-            MallImg.allImg[middleImgI].votes++
-        }
-
-        else {
-            MallImg.allImg[rightImgI].votes++
-        }
-
-
-    } else {
-        let list = document.getElementById('results-list')
-        let mallResult,
-        for (leti = 0; i < MallImg.allImg.length; i++) {
-            mallResult = document.createElement('li')
-            list.appendChild(mallResult)
-            mallResult.textContent = MallImg.allImg[i] + 'has' + MallImg.allImg[i].votes + 'votes'
-        }
-        left.removeEventListener('click', handleUserClick);
-        middle.removeEventListener('click', handleUserClick);
-        right.removeEventListener('click', handleUserClick);
-    }
-
-
-
-}
+// handleUserClick();
 
